bf4c88ad95458ef2c9bb268253c14cfdbdfe2240
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-10-21 05:00:32 +0100

    Rework project config for a header-only library

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,9 +1,8 @@
 #include <catch2/catch.hpp>
 
-#include <gryde/Public.hpp>
+#include <gryde/Matrix.hpp>
 
-using namespace com::saxbophone::gryde;
-// test case to check network construction and train movement
-TEST_CASE("Library works") {
-    REQUIRE(library_works());
+// using namespace com::saxbophone::gryde;
+TEST_CASE("Empty") {
+    SUCCEED();
 }
