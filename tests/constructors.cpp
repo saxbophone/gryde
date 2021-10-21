b5b4e3ff7c8f0b56410ffc43e346b6dd220d6ccd
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-10-21 07:21:17 +0100

    Completed all ctor tests

--- tests/constructors.cpp
+++ tests/constructors.cpp
@@ -98,3 +98,77 @@ SCENARIO("Initializer-list construction of Dynamic-size Matrx") {
         }
     }
 }
+
+SCENARIO("Span construction of Fixed-size Matrx") {
+    WHEN("A Matrix of compile-time size is constructed from a span") {
+        std::vector<int> source = {1, 2, 3, 4, 5, 6,};
+        std::span<int> view(source);
+        Matrix<int, 2, 3> matrix(view);
+        THEN("The Matrix contents are the same as those of the span") {
+            // collect Matrix contents into a vector
+            auto mc = matrix.contents();
+            std::vector<int> contents(mc.begin(), mc.end());
+            CHECK(contents == source);
+        }
+    }
+}
+
+SCENARIO("Span construction of Dynamic-size Matrx") {
+    WHEN("A Matrix of run-time size is constructed from dimensions and a span") {
+        std::vector<int> source = {1, 2, 3, 4, 5, 6,};
+        std::span<int> view(source);
+        Matrix<int> matrix(2, 3, view);
+        THEN("The Matrix has those dimensions") {
+            CHECK(matrix.row_count() == 2);
+            CHECK(matrix.col_count() == 3);
+        }
+        AND_THEN("The Matrix contents are the same as those of the span") {
+            // collect Matrix contents into a vector
+            auto mc = matrix.contents();
+            std::vector<int> contents(mc.begin(), mc.end());
+            CHECK(contents == source);
+        }
+    }
+}
+
+SCENARIO("Construct fixed-size Matrix from dynamic-size Matrix") {
+    GIVEN("A Matrix of dynamic size, with non-zero dimensions and contents") {
+        Matrix<int> dynamic(3, 2, {{1, 2,}, {3, 4,}, {1, 9,},});
+        WHEN("A Matrix of fixed size is constructed from the dynamic one") {
+            Matrix<int, 3, 2> fixed(dynamic);
+            THEN("The contents of both matrices are identical") {
+                // collect contents of both for comparison
+                auto dc = dynamic.contents();
+                auto fc = fixed.contents();
+                std::vector<int> dynamic_contents(dc.begin(), dc.end());
+                std::vector<int> fixed_contents(fc.begin(), fc.end());
+                CHECK(fixed_contents == dynamic_contents);
+            }
+        }
+    }
+}
+
+SCENARIO("Construct dynamic-size Matrix from fixed-size Matrix") {
+    GIVEN("A Matrix of fixed size, with non-zero dimensions and contents") {
+        Matrix<int, 3, 2> fixed = {
+            {1, 2,},
+            {3, 4,},
+            {1, 9,},
+        };
+        WHEN("A Matrix of dynamic size is constructed from the fixed one") {
+            Matrix<int> dynamic(fixed);
+            THEN("The dimensions of both matrices match") {
+                CHECK(dynamic.row_count() == fixed.row_count());
+                CHECK(dynamic.col_count() == fixed.col_count());
+            }
+            AND_THEN("The contents of both matrices are identical") {
+                // collect contents of both for comparison
+                auto fc = fixed.contents();
+                auto dc = dynamic.contents();
+                std::vector<int> fixed_contents(fc.begin(), fc.end());
+                std::vector<int> dynamic_contents(dc.begin(), dc.end());
+                CHECK(dynamic_contents == fixed_contents);
+            }
+        }
+    }
+}
--- tests/example.cpp
+++
@@ -1,8 +0,0 @@
-#include <catch2/catch.hpp>
-
-#include <gryde/Matrix.hpp>
-
-// using namespace com::saxbophone::gryde;
-TEST_CASE("Empty") {
-    SUCCEED();
-}
