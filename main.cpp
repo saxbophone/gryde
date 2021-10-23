e84cebb01c6b7cd36485d2a50dd950602da9a52c
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-10-23 15:19:28 +0100

    Commented out code in test program for now

--- main.cpp
+++ main.cpp
@@ -4,17 +4,19 @@
 
 
 int main() {
-    constexpr Matrix<int, 3, 3> a = {
-        {1, 9, 6,},
-        {3, 2, 7,},
-        {8, 1, 0,},
-    };
-    constexpr auto b = a.submatrix(2, 2);
-    for (auto item : b.contents()) {
-        std::cout << item << ", ";
-    }
-    std::cout << std::endl;
-    constexpr int determinant = a.determinant();
-    constexpr int b_determinant = b.determinant();
-    std::cout << determinant << " " << b_determinant << std::endl;
+    // constexpr isn't working on Windows right now
+    // needs more investigation into why this is
+    // constexpr Matrix<int, 3, 3> a = {
+    //     {1, 9, 6,},
+    //     {3, 2, 7,},
+    //     {8, 1, 0,},
+    // };
+    // constexpr auto b = a.submatrix(2, 2);
+    // for (auto item : b.contents()) {
+    //     std::cout << item << ", ";
+    // }
+    // std::cout << std::endl;
+    // constexpr int determinant = a.determinant();
+    // constexpr int b_determinant = b.determinant();
+    // std::cout << determinant << " " << b_determinant << std::endl;
 }
