11f5343c1426c86a29034cdd6b26a696cb838229
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-10-23 16:00:08 +0100

    Started adding some constexpr checks

---
+++ tests/constexpr.cpp
@@ -0,0 +1,76 @@
+#include <catch2/catch.hpp>
+
+#include <gryde/Matrix.hpp>
+
+
+TEST_CASE("constexpr default initialisation") {
+    constexpr Matrix<int, 3, 2> matrix;
+    SUCCEED();
+}
+
+TEST_CASE("constexpr list initialisation") {
+    constexpr Matrix<int, 4, 2> matrix = {
+        {9, 1,},
+        {7, 3,},
+        {2, 1,},
+        {8, 2,},
+    };
+    SUCCEED();
+}
+
+TEST_CASE("constexpr copy initialisation") {
+    constexpr Matrix<int, 9, 7> a;
+    constexpr Matrix<int, 9, 7> b = a;
+    SUCCEED();
+}
+
+TEST_CASE("constexpr get dimensions") {
+    constexpr Matrix<int, 4, 5> matrix;
+    STATIC_REQUIRE(matrix.row_count() == 4);
+    STATIC_REQUIRE(matrix.col_count() == 5);
+}
+
+TEST_CASE("constexpr equality comparison") {
+    constexpr Matrix<int, 2, 3> a = {
+        {1, 3, 7,},
+        {5, 9, 4,},
+    };
+    constexpr Matrix<int, 2, 3> b = {
+        {1, 3, 7,},
+        {5, 9, 4,},
+    };
+    STATIC_REQUIRE(a == b);
+    STATIC_REQUIRE(not (a != b));
+}
+
+TEST_CASE("constexpr get contents") {
+    constexpr Matrix<int, 4, 2> matrix = {
+        {9, 1,},
+        {7, 3,},
+        {2, 1,},
+        {8, 2,},
+    };
+    // ??? --assigning the result to a constexpr is an error, but static check is fine?
+    STATIC_REQUIRE(matrix.contents().size() == 8);
+}
+
+TEST_CASE("constexpr cell access") {
+    constexpr Matrix<int, 4, 2> matrix = {
+        {9, 1,},
+        {7, 3,},
+        {2, 1,},
+        {8, 2,},
+    };
+    constexpr int cell = matrix(1, 1);
+    CHECK(cell == 3);
+}
+
+TEST_CASE("constexpr determinant") {
+    constexpr Matrix<int, 3, 3> matrix = {
+        {1, 3, 7,},
+        {5, 9, 4,},
+        {5, 6, 4,},
+    };
+    constexpr int determinant = matrix.determinant();
+    CHECK(determinant == -93);
+}
